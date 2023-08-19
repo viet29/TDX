552a3149e07b3de4e1d062eeeca235209181808d
viet29 <quocviet.pham291@gmail.com>
2023-08-20 04:39:16 +0700

    Add some Article Module

---
+++ Client/src/app/components/admin/modals/article-preview-modal/article-preview-modal.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Article } from 'src/app/models/article';
+import { ArticleService } from 'src/app/services/article.service';
+
+@Component({
+  selector: 'app-article-preview-modal',
+  templateUrl: './article-preview-modal.component.html',
+  styleUrls: ['./article-preview-modal.component.css']
+})
+export class ArticlePreviewModalComponent implements OnInit {
+
+  article!: Article
+
+  constructor(protected bsModalRef: BsModalRef, private articleService: ArticleService) {
+
+  }
+
+  ngOnInit(): void {
+    this.articleService.getArticleById(this.article.id).subscribe({
+      next: article => {
+        this.article = article;
+      }
+    })
+  }
+
+
+}
